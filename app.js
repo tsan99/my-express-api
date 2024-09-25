97df00aabf22ffc368ae4b2aa262bfd31d2e5f32
tsan99 <91439558+tsan99@users.noreply.github.com>
2024-09-24 19:12:15 -0700

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, tsan!');
+  res.send('Hi User, this is a test API, you have initiated a GET request at ' + Date());
 });
 
 app.listen(port, () => {
